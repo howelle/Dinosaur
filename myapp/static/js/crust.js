2491d4f4cc5f2ea57d8b092d6d190c70f55ccbef
Erin Howell <howelle@reed.edu>
2016-07-13 13:53:54 -0700

    Resizing navbar,  allowing dropdown, creating new dropdown

--- myapp/static/js/crust.js
+++ myapp/static/js/crust.js
@@ -1,19 +1,24 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function myFunction(){
-	document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick=function(event) {
-	if(!event.target.matches('.dropbtn')){
-		var dropdowns = document.getElementByClassName("dropdown-content");
-		var i;
-		for (i = 0; i < dropdowns.length; i++) {
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
+	function dinoFunction(){
+		document.getElementById("Dinosaurs").classList.toggle("show");
 	}
+	function timeFunction(){
+		document.getElementById("Timeperiod").classList.toggle("show");
+	}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
+$('#menucontainer').click(function(event){
+    event.stopPropagation();
+});
