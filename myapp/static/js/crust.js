a297379922e21cf611d3fbddd4693f8d97f0ea5b
Erin Howell <howelle@reed.edu>
2016-07-13 15:16:15 -0700

    adding another d dropdown and a timeline

--- myapp/static/js/crust.js
+++ myapp/static/js/crust.js
@@ -6,10 +6,13 @@ toggle between hiding and showing the dropdown content */
 	function timeFunction(){
 		document.getElementById("Timeperiod").classList.toggle("show");
 	}
+	function placeFunction(){
+		document.getElementById("Place").classList.toggle("show");
+	}
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var dropdowns = document.getElementsByClassName("toggleable");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
@@ -22,3 +25,4 @@ window.onclick = function(event) {
 $('#menucontainer').click(function(event){
     event.stopPropagation();
 });
+
